edab15c194a85c21a9a58f2a68a641147c15cff5
Lucio Delelis <me@ldelelis.dev>
2022-11-03 16:37:18 -0300

    lowers part preview dragoon opacity

--- app/components/DragoonGeneratorSelector/DragoonGeneratorSelector.tsx
+++ app/components/DragoonGeneratorSelector/DragoonGeneratorSelector.tsx
@@ -16,12 +16,15 @@ const DragoonGeneratorSelector: FC<DragoonitemSelectorProps> = ({
   return (
     <div className="m-2 border-4 border-black grid grid-cols-1 bg-white">
       <DragoonItemPreview image={imagePath} styleProps="stacked z-20" />
-      <DragoonItemPreview styleProps="stacked z-10" image={baseBlackLine} />
+      <DragoonItemPreview
+        styleProps="stacked z-10 opacity-50"
+        image={baseBlackLine}
+      />
       {/*We override the style definitions to disable the preview's outline*/}
       <DragoonItemBase
         className="stacked "
         color={DEFAULT_COLOR}
-        style={{ fillOpacity: 0.5 }}
+        style={{ fillOpacity: 0.3 }}
       />
       <h1 className="text-center capitalize font-bold row-span-1">
         {partName}
