d20ec10cd05fdf7fb76da0e8bfdb48688708d418
tastelikecoke <jmerc87@gmail.com>
2022-11-12 00:22:12 +0800

    Extend ourple background

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="bg-purple-300">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
