637f02582a33b1405234c93eb9f44579622a0165
Lucio Delelis <me@ldelelis.dev>
2022-11-11 20:56:47 -0300

    fixes text overflow in dragoon card

--- app/routes/all.tsx
+++ app/routes/all.tsx
@@ -71,7 +71,7 @@ export default function All() {
 
   return (
     <div
-      className="p-12 font-dragoon text-2xl"
+      className="p-12 font-dragoon text-2xl h-screen"
       style={{
         backgroundImage: `url(${allBackground})`,
         backgroundSize: "50%",
@@ -80,8 +80,8 @@ export default function All() {
       <div className="grid grid-rows-2 grid-cols-2  gap-2">
         {dragoons.map((goon: Dragoon) => {
           return (
-            <div className="border-2 border-black backdrop-blur-sm flex flex-col flex-wrap">
-              <div key={goon.id} className="grid p-2">
+            <div className="border-2 border-black backdrop-blur-sm flex flex-row flex-wrap">
+              <div key={goon.id} className="grid p-2 basis-1/3">
                 <DragoonPreview
                   cloth={clothes[goon.clothes - 1]}
                   horn={horns[goon.horns - 1]}
@@ -94,10 +94,8 @@ export default function All() {
                   backgroundColor={goon.backgroundColor}
                 />
               </div>
-              <p>{goon.comment.author}</p>
-              <p className="min-h-0 h-full min-w-0 w-full">
-                {goon.comment.comment}
-              </p>
+              <p className="basis-2/3">{goon.comment.comment}</p>
+              <p className="">{goon.comment.author}</p>
             </div>
           );
         })}
