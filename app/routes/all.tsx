6ccbf386f2044977a3ed058f996baa36ef83d6d7
Lucio Delelis <me@ldelelis.dev>
2022-11-11 22:02:40 -0300

    re-layouts pagination on all dragoons view

--- app/routes/all.tsx
+++ app/routes/all.tsx
@@ -62,6 +62,7 @@ export default function All() {
   const isNextDisabled = currentPage === pageCount;
 
   const handlePagination = (e) => {
+    console.log("in pagination");
     const { target } = e;
     const operation = target.getAttribute("data-nav-operation");
     const offset = operation === "next" ? 1 : -1;
@@ -71,50 +72,54 @@ export default function All() {
 
   return (
     <div
-      className="p-12 font-dragoon text-2xl h-screen"
+      className="p-12 font-dragoon text-2xl h-screen w-screen"
       style={{
         backgroundImage: `url(${allBackground})`,
         backgroundSize: "50%",
       }}
     >
-      <div className="grid grid-rows-2 grid-cols-2  gap-2">
-        {dragoons.map((goon: Dragoon) => {
-          return (
-            <div className="border-2 border-black backdrop-blur-sm flex flex-row flex-wrap">
-              <div key={goon.id} className="grid p-2 basis-1/3">
-                <DragoonPreview
-                  cloth={clothes[goon.clothes - 1]}
-                  horn={horns[goon.horns - 1]}
-                  eye={eyes[goon.eye - 1]}
-                  handItem={handItems[goon.handItem - 1]}
-                  hat={hats[goon.hat - 1]}
-                  moustache={moustaches[goon.moustache - 1]}
-                  frame={frames[goon.frame - 1]}
-                  currentColor={goon.baseColor}
-                  backgroundColor={goon.backgroundColor}
-                />
-              </div>
-              <p className="basis-2/3">{goon.comment.comment}</p>
-              <p className="">{goon.comment.author}</p>
-            </div>
-          );
-        })}
-      </div>
-      <div>
-        <button
-          onClick={handlePagination}
-          disabled={isPreviousDisabled}
+      <img src="/banners/message-board.png" className="w-1/4 m-auto mb-12" />
+      <div className="flex flex-row gap-x-8">
+        <div
+          className="m-auto"
+          onClick={() => handlePagination}
           data-nav-operation="previous"
         >
-          Previous
-        </button>
-        <button
-          onClick={handlePagination}
-          disabled={isNextDisabled}
+          <img src="/buttons/next-button.png" className="-scale-x-100" />
+        </div>
+        <div className="grid grid-rows-2 grid-cols-2  gap-2">
+          {dragoons.map((goon: Dragoon) => {
+            return (
+              <div
+                key={goon.id}
+                className="border-2 border-black backdrop-blur-sm flex flex-row flex-wrap"
+              >
+                <div className="grid p-2 basis-1/3">
+                  <DragoonPreview
+                    cloth={clothes[goon.clothes - 1]}
+                    horn={horns[goon.horns - 1]}
+                    eye={eyes[goon.eye - 1]}
+                    handItem={handItems[goon.handItem - 1]}
+                    hat={hats[goon.hat - 1]}
+                    moustache={moustaches[goon.moustache - 1]}
+                    frame={frames[goon.frame - 1]}
+                    currentColor={goon.baseColor}
+                    backgroundColor={goon.backgroundColor}
+                  />
+                </div>
+                <p className="basis-2/3">{goon.comment.comment}</p>
+                <p className="">{goon.comment.author}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className="m-auto"
+          onClick={() => handlePagination}
           data-nav-operation="next"
         >
-          Next
-        </button>
+          <img src="/buttons/next-button.png" />
+        </div>
       </div>
     </div>
   );
